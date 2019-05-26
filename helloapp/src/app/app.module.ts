e549a493bc1f21095b2696ecdac172f1113821b2
Adya Shukla <adyashukla123.as@gmail.com>
2019-05-26 22:38:52 +0530

    Angular Demo

---
+++ helloapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule , Routes} from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import {AuthenticateServiceService} from './authenticate-service.service';
+import { HighlightDirective } from './highlight.directive'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HighlightDirective
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
+  ],
+  providers: [AuthenticateServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
